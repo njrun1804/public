c4def1472dd5142d40efa734b7dc58e5964bc754
Mike Edwards <mikeedwards@Mikes-MBP.local>
2025-01-04 14:31:36 -0500

    Add new Xcode project

---
+++ Test/Test/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Test
+//
+//  Created by Mike Edwards on 1/4/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
